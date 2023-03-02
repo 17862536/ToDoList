a1b8a3830fa8d7e589873179890f61e74dd8dcc2
Prem Kumar <116952162+17862536@users.noreply.github.com>
2023-03-02 23:35:39 +0530

    testingToDo

---
+++ frontened/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo } from "./utils/HandleApi";
+
+function App() {
+  const [toDo,setToDo]=useState([])
+  const [text,setText]=useState("")
+  useEffect(()=>{
+    getAllToDo(setToDo)
+   
+    
+    
+  },[])
+  
+  return (
+    <div className="App">
+   <div className="container">
+    <h1>ToDoApp</h1>
+    <div className="top">
+      <input 
+      type="text"
+       placeholder="Add Todos..."
+       value={text}
+       onChange={(e)=> setText (e.target.value)}
+       />
+      <div className="add" onClick={addToDo}>Add</div>
+     </div>
+    
+    <div className="list">
+      {toDo.map((item)=><ToDo key={item._id} text={item.text}/>)}
+   
+   </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontened/src/utils/HandleApi.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+const baseUrl ="http://localhost:5000"
+
+const getAllToDo =  (setToDo)=>{
+    axios
+    .get(baseUrl)
+    .then(({data})=> {
+        console.log('data--->',data);
+        setToDo(data)
+
+    })
+   
+    }
+    const addToDo =(text,setText,setToDo)=>{
+
+    }
+
+
+
+
+export{getAllToDo,addToDo}
